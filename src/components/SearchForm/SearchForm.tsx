8b13d5919a0d44920cdd1cf38ce9987fc10e39bb
nasti <anastasiasaienko634@gmail.com>
2025-09-11 19:25:09 +0200

    update all

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,11 +1,34 @@
+import { ErrorMessage, Formik } from "formik";
+import { Field, Form } from "formik";
+import type { FormikHelpers } from "formik";
+import * as Yup from "yup";
+import css from "./SearchForm.module.css";
+
 interface SearchFormProps {
   onSubmit: (topic: string) => void;
 }
 
-export default function SearchForm({ onSubmit }: SearchFormProps) {
-  const handleSubmit = (formData: FormData) => {
-    const topic = formData.get("topic") as string;
+interface initialValuesProps {
+  topic: string;
+}
 
+export default function SearchForm({ onSubmit }: SearchFormProps) {
+  const initialValues: initialValuesProps = {
+    topic: "",
+  };
+
+  const Schema = Yup.object().shape({
+    topic: Yup.string()
+      .min(2, "Topic too short")
+      .max(50, "Topic too long")
+      .required("Topic is required"),
+  });
+  const handleSubmit = (
+    values: initialValuesProps,
+    formikHelpers: FormikHelpers<initialValuesProps>
+  ) => {
+    const topic = values.topic;
+    formikHelpers.resetForm();
     if (topic === "") {
       alert("Please enter search topic!");
       return;
@@ -13,10 +36,25 @@ export default function SearchForm({ onSubmit }: SearchFormProps) {
 
     onSubmit(topic);
   };
+
   return (
-    <form action={handleSubmit}>
-      <input type="text" name="topic" />
-      <button type="submit">Search</button>
-    </form>
+    <Formik
+      validationSchema={Schema}
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+    >
+      <Form className={css.form}>
+        <Field
+          className={css.fieldset}
+          type="text"
+          name="topic"
+          placeholder="Write here something..."
+        />
+        <button className={css.button} type="submit">
+          Search
+        </button>
+        <ErrorMessage name="topic" component="span" className={css.schema} />
+      </Form>
+    </Formik>
   );
 }
